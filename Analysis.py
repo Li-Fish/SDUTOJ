a2c563ad030291fdd9ce5e528ba5b312843e7325
Fish <1136727300@qq.com>
2017-03-12 21:35:27 +0800

    修复之前的错误版本
    	已修复Bug：
    		上一版本无法正常登录，现在让Down模块先访问下主页初始化cookie即可解决问题。
    		部分Down访问网页时未使用自己定义的Session。
    
    	新增代码：
    		Down模块：
    			添加get_accepted_code和accepted_problem方法，用来从acmfish.top读取已AC代码并提交。
    		Analysis模块：
    			完善了profile方法，实现了获取用户nick和uid的功能。
    			新加了get_contest方法，获取比赛的题目列表

--- Analysis.py
+++ Analysis.py
@@ -3,16 +3,32 @@ from bs4 import BeautifulSoup
 import Down
 import re
 
+
 class Analysis(object):
     def __init__(self, user_name, password):
         self.downer = Down.Down(user_name, password)
+        self.url_home = 'http://acm.sdut.edu.cn/'
+        self.url_contest = 'http://acm.sdut.edu.cn/onlinejudge2/index.php/Home/Contest/problemlist/cid/'
+        self.url_contest_ranklist = 'http://acm.sdut.edu.cn/onlinejudge2/index.php/Home/Contest/contestranklist/cid/'
+        self.url_contest_status = 'http://acm.sdut.edu.cn/onlinejudge2/index.php/Home/Contest/conteststatus/'
+        self.user_nick, self.uid = self.profile()
 
+    # 获得用户的主页链接及昵称
     def profile(self):
-        html = self.downer.get_html("http://acm.sdut.edu.cn/onlinejudge2/index.php/Home/Contest/problemlist/cid/2022")
-        soup = BeautifulSoup(html, "html5lib")
-        print soup
+        html = self.downer.get_html("http://acm.sdut.edu.cn/onlinejudge2")
+        soup = BeautifulSoup(html, "html5lib").find_all("a")[7]
+        nick = soup.text
+        uid = re.search(r'\d+.html', soup['href']).group()[:-5]
+        return nick, uid
+
+    # 获取比赛的题目列表
+    def get_contest(self, cid):
+        html = self.downer.get_html(self.url_contest + str(cid))
+        soup = BeautifulSoup(html, 'html5lib')
+        problem_list = []
+        for x in soup.find_all('tr')[2:]:
+            problem_list.append(x.td.a['href'][-4:])
+        return problem_list
 
 if __name__ == "__main__":
-    oj = Analysis("16110543049", "FS109412")
-    oj.downer.login()
-    oj.profile()
+    oj = Analysis("test_234", "FS109412")
--- Down.py
+++ Down.py
@@ -4,31 +4,32 @@ import requests
 
 class Down(object):
     def __init__(self, user_name, password):
-        #初始化常用URL
+        # 初始化常用URL
         self.url_home = 'http://acm.sdut.edu.cn/onlinejudge2'
         self.url_login = '/index.php/Home/Login/login.html'
         self.url_submit_problem = '/index.php/Home/Solution/submitsolution'
         self.url_submit_contest = '/index.php/Home/Contest/contestsubmit/cid/'
 
-        #初始化配置
+        # 初始化配置
         self.user_name = user_name
         self.password = password
         self.session = requests.session()
-        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+        self.session.get(self.url_home)
+        self.headers = {'User-Agent': 'Mozilla/5.0'}
         self.login()
 
     def login(self):
-        requests.post(url="http://210.44.176.195/onlinejudge2/index.php/Login/login.html",
+        self.session.post(url=self.url_home + self.url_login,
                           data={'user_name':self.user_name, 'password':self.password},
                           headers=self.headers)
 
     def submit_problem(self, pid, code, cid=None, lang='g++'):
         post_data = {'pid': pid, 'lang': lang, 'code': code}
 
-        #反馈信息
+        # 反馈信息
         information = 'pid:' + str(pid) + ' Success'
 
-        #区分在Problems提交和Contest里提交
+        # 区分在Problems提交和Contest里提交
         if cid is None:
             url = self.url_home + self.url_submit_problem
         else:
@@ -41,7 +42,7 @@ class Down(object):
         return information
 
     def get_html(self, url):
-        return requests.get(url, headers=self.headers).content
+        return self.session.get(url, headers=self.headers).content
 
     def add_discuss(self, title, content, pid=1000):
         self.session.get(url=self.url_home + '/index.php/Home/Discuss/addtopic',
@@ -49,8 +50,17 @@ class Down(object):
                          headers=self.headers)
         return "Success"
 
+    def get_accepted_code(self, pid):
+        return self.session.get('http://acmfish.top/' + pid).content
+
+    def accepted_problem(self, pid, cid=None, lang='g++'):
+        code = self.get_accepted_code(pid)
+        if code == 'File not find':
+            return 'Code not find'
+
+        self.submit_problem(pid, code, cid, lang)
+        return 'Accepted'
 
 if __name__ == '__main__':
     oj = Down("test_234", "FS109412")
-    oj.login()
-    #print oj.get_html("http://acm.sdut.edu.cn/onlinejudge2/index.php/Home/Contest/problemlist/cid/2022")
+    print oj.accepted_problem(1000)
